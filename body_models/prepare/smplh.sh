e5a42b5ec9d285a32a9fe3ee885269ceca9b35cc
Shashank Tripathi <shashank.tripathi123@gmail.com>
2024-12-08 23:47:44 +0100

    updating prepare scripts

--- body_models/prepare/smplh.sh
+++ body_models/prepare/smplh.sh
@@ -18,8 +18,10 @@ echo "Clean and merge models"
 echo
 
 python body_models/prepare/merge_smplh_mano.py --smplh-fn body_models/smplh/tmp/male/model.npz --mano-left-fn body_models/smplh/tmp/mano_v1_2/models/MANO_LEFT.pkl --mano-right-fn body_models/smplh/tmp/mano_v1_2/models/MANO_RIGHT.pkl --output-folder body_models/smplh/
+python body_models/prepare/merge_smplh_mano.py --smplh-fn body_models/smplh/tmp/mano_v1_2/models/SMPLH_male.pkl --mano-left-fn body_models/smplh/tmp/mano_v1_2/models/MANO_LEFT.pkl --mano-right-fn body_models/smplh/tmp/mano_v1_2/models/MANO_RIGHT.pkl --output-folder body_models/smplh/
 
 python body_models/prepare/merge_smplh_mano.py --smplh-fn body_models/smplh/tmp/female/model.npz --mano-left-fn body_models/smplh/tmp/mano_v1_2/models/MANO_LEFT.pkl --mano-right-fn body_models/smplh/tmp/mano_v1_2/models/MANO_RIGHT.pkl --output-folder body_models/smplh/
+python body_models/prepare/merge_smplh_mano.py --smplh-fn body_models/smplh/tmp/mano_v1_2/models/SMPLH_female.pkl --mano-left-fn body_models/smplh/tmp/mano_v1_2/models/MANO_LEFT.pkl --mano-right-fn body_models/smplh/tmp/mano_v1_2/models/MANO_RIGHT.pkl --output-folder body_models/smplh/
 
 python body_models/prepare/merge_smplh_mano.py --smplh-fn body_models/smplh/tmp/neutral/model.npz --mano-left-fn body_models/smplh/tmp/mano_v1_2/models/MANO_LEFT.pkl --mano-right-fn body_models/smplh/tmp/mano_v1_2/models/MANO_RIGHT.pkl --output-folder body_models/smplh/
 
