f066a4d445e01744a68bd53244215f7feee9f6f1
hansen1416 <L.Han2@uni.brighton.ac.uk>
2026-01-15 20:50:09 +0000

    1

--- humos/src/data/motion.py
+++ humos/src/data/motion.py
@@ -7,9 +7,15 @@ from humos.prepare.tools import canonicalize_smplh, save_meshes
 from humos.prepare.extract_features import get_input_features
 from humos.utils.rotation_conversions import matrix_to_rotation_6d
 
+
 class AMASSMotionLoader:
     def __init__(
-            self, base_dir, fps, normalizer=None, disable: bool = False, canonicalize_crops: bool = False
+        self,
+        base_dir,
+        fps,
+        normalizer=None,
+        disable: bool = False,
+        canonicalize_crops: bool = False,
     ):
         self.fps = fps
         self.base_dir = base_dir
@@ -17,7 +23,7 @@ class AMASSMotionLoader:
         self.normalizer = normalizer
         self.disable = disable
         self.canonicalize_crops = canonicalize_crops
-        self.num_frames = 200 # have at num_frames many frames in the motion crops
+        self.num_frames = 200  # have at num_frames many frames in the motion crops
 
     def __call__(self, path, start, end):
         if self.disable:
@@ -42,8 +48,8 @@ class AMASSMotionLoader:
                 prev_begin = 0
                 next_end = next_end + (self.num_frames - (next_end - prev_begin))
             # if not enough frames after end
-            if next_end > len(self.motions[path]['trans']):
-                next_end = len(self.motions[path]['trans'])
+            if next_end > len(self.motions[path]["trans"]):
+                next_end = len(self.motions[path]["trans"])
         else:
             prev_begin = begin
             next_end = end
@@ -54,33 +60,51 @@ class AMASSMotionLoader:
 
         # if number of frames between begin and end are less than num_frame, repeat the last frame
         if len(motion["trans"]) < self.num_frames:
-            for k, v, in motion.items():
+            for (
+                k,
+                v,
+            ) in motion.items():
                 # get ndims
                 ndims = len(v.shape)
                 # handle root_diff and root_orient_diffs
-                if 'root_trans_diff' in k: # if velocity, then append zeros at the end
-                    motion[k] = torch.cat((v, torch.zeros(self.num_frames - len(v), v.shape[1])) , dim=0)
-                elif 'rot_orient_diff' in k: # if rot_orint_diff, append identity 6d pose at the end
+                if "root_trans_diff" in k:  # if velocity, then append zeros at the end
+                    motion[k] = torch.cat(
+                        (v, torch.zeros(self.num_frames - len(v), v.shape[1])), dim=0
+                    )
+                elif (
+                    "rot_orient_diff" in k
+                ):  # if rot_orint_diff, append identity 6d pose at the end
                     identity = torch.eye(3).repeat(self.num_frames - len(v), 1, 1)
                     # convert to 6d
                     identity = matrix_to_rotation_6d(identity)
                     motion[k] = torch.cat((v, identity), dim=0)
-                elif "vel" in k or 'angular' in k:
+                elif "vel" in k or "angular" in k:
                     raise NotImplementedError
                 else:
-                    motion[k] = torch.cat((v, v[-1].repeat(self.num_frames - len(v), *([1] * (ndims - 1)))), dim=0)
+                    motion[k] = torch.cat(
+                        (
+                            v,
+                            v[-1].repeat(
+                                self.num_frames - len(v), *([1] * (ndims - 1))
+                            ),
+                        ),
+                        dim=0,
+                    )
 
         # canonicalize crops to always start from origin and facing the same direction
         if self.canonicalize_crops:
-            motion = canonicalize_smplh(motion, ground_the_human=False, in_format='aa')
+            motion = canonicalize_smplh(motion, ground_the_human=False, in_format="aa")
 
         motion = get_input_features(motion, training=True)
+
+        print(motion)
+        exit()
 
         if self.normalizer is not None:
             motion = self.normalizer(motion)
 
         # add motion dict ot x_dict
-        x_dict = {"length": len(motion['trans'])}
+        x_dict = {"length": len(motion["trans"])}
         x_dict.update(motion)
         return x_dict
 
@@ -99,9 +123,13 @@ class Normalizer:
         std = {}
         for f in os.listdir(self.base_dir):
             if f.endswith("_mean.pt"):
-                mean[f.replace("_mean.pt", "")] = torch.load(os.path.join(self.base_dir, f))
+                mean[f.replace("_mean.pt", "")] = torch.load(
+                    os.path.join(self.base_dir, f)
+                )
             if f.endswith("_std.pt"):
-                std[f.replace("_std.pt", "")] = torch.load(os.path.join(self.base_dir, f))
+                std[f.replace("_std.pt", "")] = torch.load(
+                    os.path.join(self.base_dir, f)
+                )
         self.mean = mean
         self.std = std
 
